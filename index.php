7bd3c1eba381e3c3ae61e2d4e708ff6d509f3a09
Gaurav Kumar <dbl.gaurav@gmail.com>
2024-03-04 13:08:51 +0000

    test

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 
 
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 require_once ('soapclient/SforceEnterpriseClient.php'); // Include the Salesforce PHP Toolkit
 
